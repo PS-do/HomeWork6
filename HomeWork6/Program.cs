6dfa5b92905d18699db111e9139b716eb424e308
Сергей Плюхин <psg.pgs@mail.ru>
2022-11-10 21:51:05 +0300

    Добавьте файлы проекта.

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.    Обязательная задача*. \n" +
+                "Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом.\n" +
+                " Знаки препинания не используются. Найти самое длинное слово в строке.");
+            string str = Console.ReadLine();
+            string[] strArray = str.Split();
+            string max = strArray[0];
+            foreach (string s  in strArray)
+            {
+                if(s.Length>max.Length)
+                {
+                    max = s;
+                }
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2.    Обязательная задача*.\n" +
+                " Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом.\n" +
+                " Знаки препинания не используются. Составить программу, определяющую является ли строка палиндромом\n" +
+                " без учёта пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»).\n\n" +
+                "Введите исходную строку");
+            string startStr=Console.ReadLine().ToLower().Replace(" ", "");
+            string endStr = "";
+            foreach (char s in startStr) endStr = s + endStr;            
+            Console.WriteLine(endStr);
+            if (endStr == startStr) Console.WriteLine("Является ли строка палиндромомбез учёта пробелов и регистра? Является!");
+            else Console.WriteLine("Является ли строка палиндромомбез учёта пробелов и регистра? Не является!");
+
+        }
+    }
+}
